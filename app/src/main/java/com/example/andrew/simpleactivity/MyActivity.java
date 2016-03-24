49a6c633e4d7f1d76dd622ee1a7fb670de2bb4d8
Andrey <Darnatorg@gmail.com>
2016-03-24 22:34:48 +0200

    Initial commit

---
+++ app/src/main/java/com/example/andrew/simpleactivity/MyActivity.java
@@ -0,0 +1,59 @@
+package com.example.andrew.simpleactivity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
+public class MyActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ImageLoader imageLoader = ImageLoader.getInstance();
+        imageLoader.init(ImageLoaderConfiguration.createDefault(this));
+        ImageView imageView = (ImageView)findViewById(R.id.imageView);
+        ImageView imageView2 = (ImageView)findViewById(R.id.imageView2);
+        ImageView imageView3 = (ImageView)findViewById(R.id.imageView3);
+        ImageView imageView4 = (ImageView)findViewById(R.id.imageView4);
+        imageLoader.displayImage("http://i68.tinypic.com/viiq68.jpg", imageView);
+        imageLoader.displayImage("http://i67.tinypic.com/2h2juyu.jpg", imageView2);
+        imageLoader.displayImage("drawable://"+R.drawable.foto3, imageView3);
+        imageLoader.displayImage("drawable://"+R.drawable.foto4, imageView4);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_my, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+
+            Toast.makeText(getApplicationContext(), R.string.button_toast, Toast.LENGTH_SHORT).show();
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
